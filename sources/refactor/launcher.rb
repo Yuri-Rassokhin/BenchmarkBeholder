6fba18de4f657495b39fb257d4727eaa05c86d88
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2025-07-23 14:28:30 +0000

    refactored: applied global run to launcher

---
+++ sources/refactor/launcher.rb
@@ -0,0 +1,42 @@
+class Launcher < FlexCartesian
+  
+def initialize(logger, config)
+  super(config.parameters)
+  @logger = logger
+  @config = config
+  setup
+end
+
+private
+
+def setup
+  result = ""
+
+  self.func(:add, :command) do |v|
+    case v.operation
+      when "read", "randread"
+        flow = "if=#{@config.target} of=/dev/null"
+      when "write", "randwrite"
+        flow = "if=/dev/zero of=#{@config.target}"
+    end
+    "#{@config.actor} #{flow} bs=#{v.size} count=#{v.count}"
+  end
+
+  self.func(:add, :result, hide: true) do |v|
+    result = Global.run(binding, v.host, proc { `#{v.command} 2>&1`.strip })
+  end
+
+  self.func(:add, :error) do |v|
+    Global.run(binding, v.host, proc { `echo "#{result.gsub(/\s+/, ' ')}" | grep error`.strip})
+  end
+
+  self.func(:add, :bandwidth) do |v|
+    Global.run(binding, v.host, proc { `echo "#{result}" | grep copied | sed -e 's/^.*,//' | awk '{print $1}'`.strip.to_f })
+  end
+
+  self.func(:add, :units) do |v|
+    Global.run(binding, v.host, proc { `echo "#{result}" | grep copied | sed -e 's/^.*,//' | awk '{print $2}'`.strip })
+  end
+end
+
+end
