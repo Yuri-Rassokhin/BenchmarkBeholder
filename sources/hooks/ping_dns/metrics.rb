79b354ec071baf486392811af5a5aa63fb2b71a5
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2025-08-09 17:09:09 +0300

    refactored: ping_dns hook changed to module format

--- sources/hooks/ping_dns/metrics.rb
+++ sources/hooks/ping_dns/metrics.rb
@@ -1,10 +1,5 @@
-class Metrics < Hook
-
-def initialize(logger, config, target)
-  super(logger, config, target)
-end
-
-private
+module Metrics
+  module_function
 
 # In the `setup` method, you specify all the target metrics BBH should calculate for each combination of parameters
 # Vector `v` refers to current combination of parameters
@@ -13,14 +8,13 @@ private
 # After that, BBH will launch the benchmarking and sweep `v` over all valid combinations of parameters
 # NOTE: If you need pre-benchmark preparation, just place it in the beginning of `setup`
 # NOTE: If you need preparation before EACH combination is benchmarked, define it as one more `func(:add, ...)` and call it from the function that invokes benchmark
-def setup
+def self.setup(space, logger, config)
   result = {} # here we'll store raw result of PING
-  func(:add, :command) { |v| "ping -c #{v.count} -s #{v.size} #{v.dns}" } # construct PING command with current combination of parameters
-  func(:add, :raw_ping, hide: true) { |v| result[v.command] ||= `#{v.command} 2>&1` } # run PING and capture its raw result
-  func(:add, :time) { |v| v.raw_ping[/min\/avg\/max\/(?:mdev|stddev) = [^\/]+\/([^\/]+)/, 1]&.to_f } # extract ping time
-  func(:add, :min) { |v| v.raw_ping[/min\/avg\/max\/(?:mdev|stddev) = ([^\/]+)/, 1]&.to_f } # extract min ping time
-  func(:add, :loss) { |v| v.raw_ping[/(\d+(?:\.\d+)?)% packet loss/, 1]&.to_f } # extract loss rate
+  space.func(:add, :command) { |v| "ping -c #{v.count} -s #{v.size} #{v.dns}" } # construct PING command with current combination of parameters
+  space.func(:add, :raw_ping, hide: true) { |v| result[v.command] ||= `#{v.command} 2>&1` } # run PING and capture its raw result
+  space.func(:add, :time) { |v| v.raw_ping[/min\/avg\/max\/(?:mdev|stddev) = [^\/]+\/([^\/]+)/, 1]&.to_f } # extract ping time
+  space.func(:add, :min) { |v| v.raw_ping[/min\/avg\/max\/(?:mdev|stddev) = ([^\/]+)/, 1]&.to_f } # extract min ping time
+  space.func(:add, :loss) { |v| v.raw_ping[/(\d+(?:\.\d+)?)% packet loss/, 1]&.to_f } # extract loss rate
 end
 
 end
-
