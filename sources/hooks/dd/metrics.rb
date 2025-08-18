7fcb17fbcaf838f36294893e9968a64f9d239154
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2025-08-18 14:30:20 +0000

    polished: removed legacy code

--- sources/hooks/dd/metrics.rb
+++ sources/hooks/dd/metrics.rb
@@ -3,17 +3,6 @@ require 'tempfile'
 module Metrics
   module_function
 
-def file_init(logger, config)
-  size = config[:workload][:total_size]
-  file = config.target
-
-  @logger.info "creating target file #{file} of the size #{size}, rounded to megabytes"
-  File.open(file, "wb") do |f|
-      block = "\0" * 1024 * 1024  # 1MB
-      (size / block.size).times { f.write(block) }
-  end
-end
-
 def setup(space, logger, config, target)
   result = ""
 
