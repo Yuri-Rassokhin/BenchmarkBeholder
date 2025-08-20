8b2d569e685ee731302d576cf3de8243966c891d
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2025-08-20 21:12:24 +0000

    simplification in progress

--- sources/infra/scheduler.rb
+++ sources/infra/scheduler.rb
@@ -2,11 +2,9 @@ module Scheduler
 
 def self.prepare(logger, config)
   conf_schedulers = Set.new(config.schedulers)
-  config.hosts.each do |host|
-    host_schedulers = Set.new(Global.run(binding, host, Scheduler.method(:schedulers)).split)
-    diff = conf_schedulers ^ host_schedulers
-    logger.warn "IO scheduler(s) '#{diff.join(", ")}' differ from config on '#{host}'" unless diff.empty?
-
+  host_schedulers = Set.new(Scheduler.schedulers.split)
+  diff = conf_schedulers ^ host_schedulers
+  logger.warn "IO schedulers #{diff.join(", ")} are not in sweep file" unless diff.empty?
   end
 end
 
@@ -16,20 +14,21 @@ def self.switch(logger, scheduler, volumes)
     begin
       `sudo bash -c "echo #{scheduler} > /sys/block/#{base_device(v)}/queue/scheduler"`
     rescue => e
-      logger.error("Failed to switch IO scheduler for #{v}: #{e.message}")
+      logger.error("failed to switch IO scheduler for #{v}: #{e.message}")
     end
   end
 end
 
+class << self
 private
 
-def self.base_device(dev_path)
+def base_device(dev_path)
   dev = File.basename(dev_path)
   output = `lsblk -no PKNAME /dev/#{dev}`.lines.first&.strip
   output.empty? ? dev : output
 end
 
-def self.schedulers
+def schedulers
   `cat /sys/block/sda/queue/scheduler`.strip.gsub(/[\[\]]/, '')
 end
 
