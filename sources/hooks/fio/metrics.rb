68795c40ab3c75156ba857dfbefdd40f232b3d98
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2025-08-23 20:21:01 +0300

    polished: progress logging

--- sources/hooks/fio/metrics.rb
+++ sources/hooks/fio/metrics.rb
@@ -3,14 +3,10 @@ class Workload
 private
 
 def setup
-  temp :raw
-  temp :command
-  temp :infra, {}
-
   # NOTE: so far, indirect access is hardcoded for A). simplicity (to avoid validity checks with with ioengines, and B. practical need
-  func(:add, :command) { |v| @command ||= "fio --direct=0 --rw=#{v.operation} --bs=#{v.size} --ioengine=#{v.ioengine} --iodepth=#{v.iodepth} --output-format=json --numjobs=#{v.processes} --name=bbh_fio --filename=#{@config.target}".strip }
+  func(:add, :command) { |v| @temp[:command] ||= "fio --direct=0 --rw=#{v.operation} --bs=#{v.size} --ioengine=#{v.ioengine} --iodepth=#{v.iodepth} --output-format=json --numjobs=#{v.processes} --name=bbh_fio --filename=#{@config.target}".strip }
 
-  func(:add, :raw, hide: true) { |v| Scheduler.switch(@logger, v.scheduler, @volumes); @raw = `#{v.command}`.strip }
+  func(:add, :raw, hide: true) { |v| Scheduler.switch(@logger, v.scheduler, @volumes); @temp[:raw] = `#{v.command}`.strip }
 
   func(:add, :iops) { |v| @result[:iops] ||= JSON.parse(v.raw)["jobs"][0][v.operation]["iops"].to_i }
 
@@ -20,7 +16,8 @@ def setup
   func(:add, :units) { "MB/s" }
 
   # standard functions for infrastructure metrics
-  Platform.metrics(logger: @logger, target: @config.target, space: self, result: @infra, gpu: false)
+  @temp[:infra] = {}
+  Platform.metrics(logger: @logger, target: @config.target, space: self, result: @temp[:infra], gpu: false)
 end
 
 end
