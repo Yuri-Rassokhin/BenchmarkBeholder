70fbc3001d5a7adc1e1b7bab89962f97ece47d7c
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2025-08-23 17:19:08 +0300

    polished: more flexible logging of progress and result

--- sources/hooks/fio/metrics.rb
+++ sources/hooks/fio/metrics.rb
@@ -3,23 +3,22 @@ class Workload
 private
 
 def setup
-  result = {}
+  temp(:raw)
 
   # NOTE: so far, indirect access is hardcoded for A). simplicity (to avoid validity checks with with ioengines, and B. practical need
   func(:add, :command) { |v| @result[:command] ||= "fio --direct=0 --rw=#{v.operation} --bs=#{v.size} --ioengine=#{v.ioengine} --iodepth=#{v.iodepth} --output-format=json --numjobs=#{v.processes} --name=bbh_fio --filename=#{@config.target}".strip }
 
-  func(:add, :result, hide: true) { |v| Scheduler.switch(@logger, v.scheduler, @volumes); @result[:result] = `#{v.command}`.strip }
+  func(:add, :raw, hide: true) { |v| Scheduler.switch(@logger, v.scheduler, @volumes); @raw = `#{v.command}`.strip }
 
-  func(:add, :iops) { |v| @result[:iops] ||= JSON.parse(v.result)["jobs"][0][v.operation]["iops"].to_i }
+  func(:add, :iops) { |v| @result[:iops] ||= JSON.parse(v.raw)["jobs"][0][v.operation]["iops"].to_i }
 
   # BW converted to MB/s
-  func(:add, :bandwidth) { |v| @result[:bandwidth] ||= JSON.parse(v.result)["jobs"][0][v.operation]["bw_bytes"].to_f / 1_000_000 }
+  func(:add, :bandwidth) { |v| @result[:bandwidth] ||= JSON.parse(v.raw)["jobs"][0][v.operation]["bw_bytes"].to_f / 1_000_000 }
 
   func(:add, :units) { "MB/s" }
 
   # standard functions for infrastructure metrics
-  Platform.metrics(logger: @logger, target: @config[:misc][:target], space: self, result: @result, gpu: false)
+  Platform.metrics(logger: @logger, target: @config.target, space: self, result: @result, gpu: false)
 end
 
 end
-
